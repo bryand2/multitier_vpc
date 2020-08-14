7a6adba8b01bb815f25823b84d1c3a82550d8469
Bryan Daniel <bryand@us.ibm.com>
2020-08-14 15:09:40 -0500

    updates

--- outputs.tf
+++ outputs.tf
@@ -7,5 +7,14 @@ output vpc_id {
   value       = ibm_is_vpc.vpc.id
 }
 
+output subnet_ids {
+  description = "IDs of subnets created for this tier"
+  value       = ibm_is_subnet.subnet.*.id
+}  
+
+output cidr_blocks {
+  value       = ibm_is_subnet.subnet.*.ipv4_cidr_block
+}
+
 ##############################################################################
 
