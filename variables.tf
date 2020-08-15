f7a056a14e123e2eb5db6bcc34ed9d84bbbf4d53
Bryan Daniel <bryand@us.ibm.com>
2020-08-15 14:49:23 -0500

    updated code

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@
 ##############################################################################
 
 variable ibmcloud_api_key {
-  description = "The IBM Cloud platform API key needed to deploy IAM enabled resources!!!"
+  description = "The IBM Cloud platform API key needed to deploy IAM enabled resources"
   type        = string
 }
 
